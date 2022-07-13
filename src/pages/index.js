02bd5bd4be093972b0cc57da6ec9bbf410666567
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2022-07-13 01:24:53 -0500

    fin

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,15 +18,15 @@ const index = () => {
       <div className="flex-1">
         <div className="Marketing">
           <img src="https://firebasestorage.googleapis.com/v0/b/drive-d75a7.appspot.com/o/IMG_0713.JPG?alt=media&token=b7bf0d4f-1a32-4d53-9afa-0980a7fe08a4" alt="marketing" id="mk" />
-          <h2>Marketing</h2>
-          <p>ACÁ INICIAMOS EN EL MARAVILLOSO MUNDO DEL MARKETING POR SUSCRIPCIÓN.</p>
+          <h2>d. comercial</h2>
+          <p>APRENDE A SER UN EXPERTO COMERCIAL.</p>
           <Link to={'/marketing'} className="button">Comenzar</Link>
 
         </div>
         <div className="Traiding">
           <img src="https://firebasestorage.googleapis.com/v0/b/drive-d75a7.appspot.com/o/IMG_0714.JPG?alt=media&token=abb2e5e6-fecf-4965-a616-e950189aa33d" alt="traiding" />
           <h2>Trading</h2>
-          <p>ACÁ INICIAMOS EN EL MUNDO DE LOS MERCADOS FINANCIEROS.</p>
+          <p>EL MUNDO DE LOS MERCADOS FINANCIEROS.</p>
           <Link to={'/trading'} className="button">Comenzar</Link>
         </div>
       </div>
